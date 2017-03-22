981a734bce6344ca6fb98f6f8919ac80172ea2c5
sandyre <aleksanteri@niuitmo.ru>
2017-03-22 15:33:46 +0300

    removed garbage files

--- Classes/DynamicLayer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  DynamicLayer.cpp
-//  labyrinth
-//
-//  Created by Aleksandr Borzikh on 08.03.17.
-//
-//
-
-#include "DynamicLayer.hpp"
--- Classes/light.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  light.cpp
-//  labyrinth
-//
-//  Created by Aleksandr Borzikh on 08.03.17.
-//
-//
-
-#include "light.hpp"
